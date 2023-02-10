77113bcc2cc94cedd93591054867eebd0e3151c3
ritaboury <113262405+ritaboury@users.noreply.github.com>
2023-02-10 12:04:47 -0700

    Create ex1.3.py

---
+++ ex1.3.py
@@ -0,0 +1,11 @@
+def fib(n, memo = {}): 
+
+  if n == 0 or n == 1: 
+    return n 
+
+  if n in memo: 
+    return memo[n] 
+
+  else: 
+    memo[n] = fib(n-1, memo) + fib(n-2, memo) 
+    return memo[n] 
